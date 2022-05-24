2111458746cec7c6602b4098b1ec01342ec1ece2
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-05-24 13:26:29 +0200

    Add XOR data generation functions
    These files provide an easy way of generating XOR-based
    data. Its distribution in a 2D space is very handy to
    check whether the MLP is behaving as expected. The 4
    well-defined clusters that end up forming allow a human
    (i.e. us) to have a very intuitive feedback as to what's
    going on behind the scenes.
    
    In order to avoid the entire dataset to be purely synthetic,
    we're also adding some normal noise to the basic data points.

---
+++ src/xor_data_gen.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"math/rand"
+
+	"gonum.org/v1/gonum/mat"
+)
+
+func genXor(n int, stdDev float64) []*mat.Dense {
+	data := make([]*mat.Dense, n)
+	var m mat.Dense
+	for i := 0; i < n; i++ {
+		switch i % 4 {
+		case 0:
+			data[i] = mat.NewDense(1, 2, []float64{0, 0})
+		case 1:
+			data[i] = mat.NewDense(1, 2, []float64{0, 1})
+		case 2:
+			data[i] = mat.NewDense(1, 2, []float64{1, 0})
+		case 3:
+			data[i] = mat.NewDense(1, 2, []float64{1, 1})
+		}
+
+		// Generate some normally distributed noise
+		m.Scale(stdDev, mat.NewDense(1, 2, []float64{rand.NormFloat64(), rand.NormFloat64()}))
+		data[i].Add(data[i], &m)
+	}
+	return data
+}
+
+func xorOutput(in *mat.Dense) float64 {
+	_, c := in.Dims()
+	xor := 0
+	for i := 0; i < c; i++ {
+		if in.At(0, i) > 0.5 {
+			xor++
+		}
+	}
+
+	if xor == 1 {
+		return 1
+	}
+	return 0
+}
