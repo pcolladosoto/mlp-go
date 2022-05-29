c77200b91571dc947e7b03ead671de5d12174ef0
Pablo Collado Soto <pcolladosoto@gmx.com>
2022-05-29 15:24:01 +0200

    Export necessary fields
    We also began using mixedCaps all around!

--- mlp/mnist.go
+++ mlp/mnist.go
@@ -20,44 +20,44 @@ var type_map map[uint32]string = map[uint32]string{
 	0xE: "double",
 }
 
-type labels struct {
-	magic_num      uint32
-	data_type      string
-	dimensionality uint
-	n              uint32
-	labels         []byte
+type Labels struct {
+	MagicNum       uint32
+	DataType       string
+	Dimensionality uint
+	N              uint32
+	Labels         []byte
 }
 
-type images struct {
-	magic_num      uint32
-	data_type      string
-	dimensionality uint
-	n              uint32
-	img_rows       uint32
-	img_cols       uint32
-	images         [][][]float64
+type Images struct {
+	MagicNum       uint32
+	DataType       string
+	Dimensionality uint
+	N              uint32
+	ImgRows        uint32
+	ImgCols        uint32
+	Images         [][][]float64
 }
 
-func read_labels(fpath string) (labels, error) {
+func ReadLabels(fpath string) (Labels, error) {
 	fd, err := os.Open(fpath)
 	if err != nil {
-		return labels{}, err
+		return Labels{}, err
 	}
 	defer fd.Close()
 
 	i_buff, b_buff := make([]byte, 8), make([]byte, 1)
 	if _, err := fd.Read(i_buff); err != nil {
-		return labels{}, err
+		return Labels{}, err
 	}
 
-	mnum := to_big_endian(i_buff[:4])
+	mnum := toBigEndian(i_buff[:4])
 
-	lbs := labels{
-		magic_num: mnum, n: to_big_endian(i_buff[4:]),
-		data_type: type_map[mnum&(0xFF>>8)], dimensionality: uint(mnum & 0xFF),
+	lbs := Labels{
+		MagicNum: mnum, N: toBigEndian(i_buff[4:]),
+		DataType: type_map[mnum&(0xFF>>8)], Dimensionality: uint(mnum & 0xFF),
 	}
 
-	lbs.labels = make([]byte, lbs.n)
+	lbs.Labels = make([]byte, lbs.N)
 
 	i := 0
 	for {
@@ -65,67 +65,67 @@ func read_labels(fpath string) (labels, error) {
 			if err == io.EOF {
 				return lbs, nil
 			}
-			return labels{}, err
+			return Labels{}, err
 		}
-		lbs.labels[i] = b_buff[0]
+		lbs.Labels[i] = b_buff[0]
 		i++
 	}
 }
 
-func to_big_endian(data []byte) uint32 {
+func toBigEndian(data []byte) uint32 {
 	return uint32(data[0])<<24 | uint32(data[1])<<16 | uint32(data[2])<<8 | uint32(data[3])
 }
 
-func read_imgs(fpath string) (images, error) {
+func ReadImgs(fpath string) (Images, error) {
 	fd, err := os.Open(fpath)
 	if err != nil {
-		return images{}, err
+		return Images{}, err
 	}
 	defer fd.Close()
 
 	i_buff := make([]byte, 16)
 	if _, err := fd.Read(i_buff); err != nil {
-		return images{}, err
+		return Images{}, err
 	}
 
-	mnum := to_big_endian(i_buff[:4])
+	mnum := toBigEndian(i_buff[:4])
 
-	imgs := images{
-		magic_num: mnum, n: to_big_endian(i_buff[4:8]),
-		img_rows: to_big_endian(i_buff[8:12]), img_cols: to_big_endian(i_buff[12:]),
-		data_type: type_map[mnum&(0xFF>>8)], dimensionality: uint(mnum & 0xFF),
+	imgs := Images{
+		MagicNum: mnum, N: toBigEndian(i_buff[4:8]),
+		ImgRows: toBigEndian(i_buff[8:12]), ImgCols: toBigEndian(i_buff[12:]),
+		DataType: type_map[mnum&(0xFF>>8)], Dimensionality: uint(mnum & 0xFF),
 	}
 
-	imgs.images = make([][][]float64, imgs.n)
-	for i := range imgs.images {
-		imgs.images[i] = make([][]float64, imgs.img_rows)
-		for j := range imgs.images[i] {
-			imgs.images[i][j] = make([]float64, imgs.img_cols)
+	imgs.Images = make([][][]float64, imgs.N)
+	for i := range imgs.Images {
+		imgs.Images[i] = make([][]float64, imgs.ImgRows)
+		for j := range imgs.Images[i] {
+			imgs.Images[i][j] = make([]float64, imgs.ImgCols)
 		}
 	}
 
 	i := 0
-	b_buff := make([]byte, imgs.img_rows*imgs.img_cols)
+	b_buff := make([]byte, imgs.ImgRows*imgs.ImgCols)
 	for {
 		if _, err := fd.Read(b_buff); err != nil {
 			if err == io.EOF {
 				return imgs, nil
 			}
-			return images{}, err
+			return Images{}, err
 		}
-		for j := 0; j < int(imgs.img_rows); j++ {
-			for k := 0; k < int(imgs.img_cols); k++ {
-				imgs.images[i][j][k] = float64(b_buff[j*int(imgs.img_cols)+k]) / 255
+		for j := 0; j < int(imgs.ImgRows); j++ {
+			for k := 0; k < int(imgs.ImgCols); k++ {
+				imgs.Images[i][j][k] = float64(b_buff[j*int(imgs.ImgCols)+k]) / 255
 			}
 		}
 		i++
 	}
 }
 
-func dump_image(imgs images, index int, fname string) error {
-	img := image.NewRGBA(image.Rect(0, 0, int(imgs.img_cols), int(imgs.img_rows)))
+func DumpImage(imgs Images, index int, fname string) error {
+	img := image.NewRGBA(image.Rect(0, 0, int(imgs.ImgCols), int(imgs.ImgRows)))
 
-	for i, row := range imgs.images[index] {
+	for i, row := range imgs.Images[index] {
 		for j, px := range row {
 			img.SetRGBA(j, i, color.RGBA{255 - uint8(px*255), 255 - uint8(px*255), 255 - uint8(px*255), 255})
 		}
